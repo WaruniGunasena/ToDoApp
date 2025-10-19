9a80a74da25f444e0602d38c5488a8e09601d95d
Waruni Gunasena <121920439+WaruniGunasena@users.noreply.github.com>
2025-10-19 15:32:32 +0530

    Complete the Todo App

--- to-do-app/src/components/TodoList.jsx
+++ to-do-app/src/components/TodoList.jsx
@@ -2,9 +2,11 @@ import TodoItems from "./TodoItems";
 import styles from './todolist.module.css'
 
 const TodoList = ({todos, setTodos}) => {
+
+  const sortedTodos = todos.slice().sort((a,b) => Number(a.done)-Number(b.done));
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItems key={item.name} item={item} todos={todos} setTodos={setTodos}/>
       ))}
     </div>
